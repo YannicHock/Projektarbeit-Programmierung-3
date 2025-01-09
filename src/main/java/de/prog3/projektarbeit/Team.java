4dec3d79c257960d76cee89f038b6cfeb779181c
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-01-09 16:21:19 +0100

    #3 Logik Grrundstruktur erstellt

---
+++ src/main/java/de/prog3/projektarbeit/Team.java
@@ -0,0 +1,11 @@
+package de.prog3.projektarbeit;
+
+public class Team {
+    private String teamName; // name of the Team
+    private String teamMembers; // Members of the Team
+
+    public Team(String teamName, String teamMembers) {}
+
+   // public String getTeamName() {}
+   // public String getTeamName() {}
+}
