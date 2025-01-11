dba83b36bb3feee5b42a3345c9378600525bd162
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-01-11 08:06:24 +0100

    Ich habe Änderung bei der Grundlogik Hinzugefügt

--- src/main/java/de/prog3/projektarbeit/Player.java
+++ src/main/java/de/prog3/projektarbeit/Player.java
@@ -1,5 +1,22 @@
 package de.prog3.projektarbeit;
 
 public class Player {
+    private String playerName; // Name of the player
+    private int playAge; // Age of the player
+    private String playerPosition; // position of the player
 
+    public Player(String playerName, int playAge, String playerPosition) {}
+
+    public void setPlayerName(String playerName) {
+    }
+
+    public void setPlayAge(int playAge) {}
+
+    public void setPlayerPosition(String playerPosition) {}
+
+   // public String getPlayerName() {}
+
+    //public String getPlayerPosition() {}
+
+    // public int getPlayAge() {}
 }
