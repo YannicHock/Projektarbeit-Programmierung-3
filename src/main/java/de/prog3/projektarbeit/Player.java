81d8c38d3acf40455b846ea9b6a4337cd886afa1
MathuSpavan <Mathusanlondon@gmail.com>
2025-01-16 14:52:03 +0100

    Beginn der Datenbank Integration

--- src/main/java/de/prog3/projektarbeit/Player.java
+++ src/main/java/de/prog3/projektarbeit/Player.java
@@ -4,11 +4,13 @@ public class Player {
     private String name; // Name of the player
     private int age; // Age of the player
     private String position; // position of the player
+    private int number;
 
-    public Player(String name, int age, String position) {
+    public Player(String name, int age, String position, int number) {
         this.name = name;
         this.age = age;
         this.position = position;
+        this.number = number;
     }
 
     public int getAge() {
@@ -22,4 +24,6 @@ public class Player {
     public String getPosition() {
         return position;
     }
+
+    public int getNumber() {return number;}
 }
--- src/main/java/de/prog3/projektarbeit/data/InsertPlayer.java
+++ src/main/java/de/prog3/projektarbeit/data/InsertPlayer.java
@@ -1,30 +1,33 @@
 package de.prog3.projektarbeit.data;
 
+import de.prog3.projektarbeit.jooq.tables.Player;
+import de.prog3.projektarbeit.jooq.tables.records.PlayerRecord;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
 
+import static de.prog3.projektarbeit.jooq.tables.Player.PLAYER;
 import static org.jooq.impl.DSL.table;
 import static org.jooq.impl.DSL.field;
 
 public class InsertPlayer {
-
-    public static void main(String[] args) {
+    public static void insertPlayer(){
         DSLContext ctx = JooqContextProvider.getDSLContext();
 
         // Insert als "Query Builder"
-        ctx.insertInto(table("players"))
-                .columns(field("id"),field("name"), field("position"))
-                .values("001","Mathusan Saravanapavan",  "Stürmer")
+        ctx.insertInto(PLAYER)
+                .columns(PLAYER.NAME, PLAYER.POSITION)
+                .values("Mathusan Saravanapavan",  "Stürmer")
                 .execute();
+
 
         System.out.println("Spieler hinzugefügt!");
 
         // Zur Kontrolle: alle Datensätze abfragen
-        Result<Record> result = ctx.select().from("players").fetch();
+        Result<Record> result = ctx.select().from(PLAYER).fetch();
         System.out.println("Anzahl Datensätze: " + result.size());
         for (Record r : result) {
-            System.out.println("Datensatz: " + r);
+            System.out.println(r.get(PLAYER.NAME) + ": " + r.get(PLAYER.POSITION));
         }
     }
 }
