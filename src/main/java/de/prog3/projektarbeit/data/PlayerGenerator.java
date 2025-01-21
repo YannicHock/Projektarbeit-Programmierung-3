0486756ee14c504c09d73cd506935a0986120b04
MathuSpavan <Mathusanlondon@gmail.com>
2025-01-21 17:17:21 +0100

    Hikary

--- src/main/java/de/prog3/projektarbeit/data/PlayerGenerator.java
+++ src/main/java/de/prog3/projektarbeit/data/PlayerGenerator.java
@@ -8,7 +8,20 @@ public class PlayerGenerator {
 
     private static final String[] NAMES = {"John", "Mike", "Sarah", "Jessica", "David", "Emily", "Chris", "Anna"};
     private static final String[] POSITIONS = {"Goalkeeper", "Defender", "Midfielder", "Forward"};
-
+//    private static final Integer[] JERSEY_NUMBER = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+//    private static final String [] BIRTHPLACE = {
+//            "Germany", "France", "Italy", "Spain", "Portugal",
+//            "Netherlands", "Belgium", "Sweden", "Denmark",
+//            "Norway", "Finland", "Poland", "Austria", "Switzerland",
+//            "United Kingdom", "Ireland", "Czech Republic", "Slovakia",
+//            "Hungary", "Romania", "Bulgaria", "Greece", "Turkey",
+//            "Russia", "Ukraine", "Croatia", "Serbia", "Bosnia and Herzegovina",
+//            "Slovenia", "Montenegro", "Albania", "North Macedonia",
+//            "United States", "Canada", "Brazil", "Argentina", "Chile",
+//            "Uruguay", "Colombia", "Mexico", "South Africa", "Egypt",
+//            "Nigeria", "Ghana", "Kenya", "China", "Japan", "South Korea",
+//            "India", "Australia", "New Zealand"
+//    };
     public static Player generateRandomPlayer() {
         Random random = new Random();
         String name = NAMES[random.nextInt(NAMES.length)];
