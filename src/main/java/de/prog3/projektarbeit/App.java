88d85a0567a1ac870d35a3db0931ed9b5ef5bedf
YannicHock <mail@yannic-hock.de>
2025-03-11 21:33:35 +0100

    Umstrukturierung der Datenbankanbindung

--- src/main/java/de/prog3/projektarbeit/App.java
+++ src/main/java/de/prog3/projektarbeit/App.java
@@ -1,7 +1,8 @@
 package de.prog3.projektarbeit;
 
 
-import de.prog3.projektarbeit.data.InsertPlayer;
+import de.prog3.projektarbeit.data.DataListener;
+import de.prog3.projektarbeit.data.DataSourceProvider;
 import de.prog3.projektarbeit.eventHandling.events.ui.RequestNewViewEvent;
 import de.prog3.projektarbeit.ui.UIHandler;
 import de.prog3.projektarbeit.ui.views.ViewType;
@@ -9,28 +10,20 @@ import de.prog3.projektarbeit.ui.views.ViewType;
 public class App {
 
     private void prepareListeners() {
+        DataListener.register();
         UIHandler.getInstance().registerListeners();
     }
 
     public void start() {
+        DataSourceProvider.init();
         prepareListeners();
         new RequestNewViewEvent(ViewType.LATERNA).call();
     }
 
 
     public static void main(String[] args) {
-        Player player = new Player("A", 12, "pos", 12);
-      InsertPlayer.insertPlayer();
         App app = new App();
         app.start();
     }
 
-
-    public int magicNumber() {
-        return 42;
-    }
-
-    public boolean allWaysFalse(){
-        return false;
-    }
 }
