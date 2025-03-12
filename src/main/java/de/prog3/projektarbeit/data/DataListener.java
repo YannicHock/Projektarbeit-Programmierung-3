0a3007fa605dcca631b517f31da7e68719b750d8
MathuSpavan <Mathusanlondon@gmail.com>
2025-03-12 15:14:21 +0100

    Datenbank kann information speichern und laden, habe zudem die zufallsgenartor gelöscht

--- src/main/java/de/prog3/projektarbeit/data/DataListener.java
+++ src/main/java/de/prog3/projektarbeit/data/DataListener.java
@@ -1,8 +1,80 @@
 package de.prog3.projektarbeit.data;
 
+import de.prog3.projektarbeit.data.factories.PlayerFactory;
+import de.prog3.projektarbeit.data.factories.TeamFactory;
+import de.prog3.projektarbeit.data.objects.Player;
+import de.prog3.projektarbeit.data.objects.Team;
+import de.prog3.projektarbeit.eventHandling.events.data.player.AttemptPlayerCreationEvent;
+import de.prog3.projektarbeit.eventHandling.events.data.player.PlayerCreationFinishedEvent;
+import de.prog3.projektarbeit.eventHandling.events.data.team.AttemptTeamCreationEvent;
+import de.prog3.projektarbeit.eventHandling.events.data.team.TeamCreationFinishedEvent;
+import de.prog3.projektarbeit.eventHandling.listeners.data.player.AttemptPlayerCreationListener;
+import de.prog3.projektarbeit.eventHandling.listeners.data.team.AttemptTeamCreationListener;
+import de.prog3.projektarbeit.exceptions.UnableToSavePlayerExeption;
+import de.prog3.projektarbeit.exceptions.UnableToSaveTeamExeption;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 
 public class DataListener {
     public static void register(){
+        new AttemptPlayerCreationListener() {
+            @Override
+            public void onEvent(AttemptPlayerCreationEvent event) {
+                Player player = null;
+                ArrayList<Exception> exceptions = new ArrayList<>();
+                try {
+                    PlayerFactory playerFactory = new PlayerFactory();
+                    player = playerFactory.setFirstName(event.getFirstName())
+                            .setLastName(event.getLastName())
+                            .setDateOfBirth(Player.parseStringToDate(event.getDateOfBirth()))
+                            .setNumber(Integer.parseInt(event.getNumber()))
+                            .setPositions(event.getPositions())
+                            .build();
+                } catch (IllegalArgumentException e) {
+                    exceptions.add(e);
+                } catch (ParseException e) {
+                    exceptions.add(new ParseException("Fehler beim Parsen des Geburtsdatums", e.getErrorOffset()));
+                }
+
+                if(player!=null){
+                    try {
+                        player.save();
+                        new PlayerCreationFinishedEvent(player).call();
+                        return;
+                    } catch (UnableToSavePlayerExeption e) {
+                        exceptions.add(e);
+                    }
+                }
+                new PlayerCreationFinishedEvent(exceptions).call();
+            }
+        };
+
+
+        new AttemptTeamCreationListener() {
+            @Override
+            public void onEvent(AttemptTeamCreationEvent event) {
+                Team team = null;
+                ArrayList<Exception> exceptions = new ArrayList<>();
+                try {
+                    TeamFactory teamFactory = new TeamFactory();
+                    team = teamFactory.setName(event.getTeamName())
+                            .build();
+                } catch (IllegalArgumentException e) {
+                    exceptions.add(e);
+                }
+
+                if(team!=null){
+                    try {
+                        team.save();
+                        new TeamCreationFinishedEvent(team).call();
+                        return;
+                    } catch (UnableToSaveTeamExeption e) {
+                        exceptions.add(e);
+                    }
+                }
+                new PlayerCreationFinishedEvent(exceptions).call();
+            }
+        };
     }
 }
--- src/main/java/de/prog3/projektarbeit/data/objects/Team.java
+++ src/main/java/de/prog3/projektarbeit/data/objects/Team.java
@@ -1,8 +1,13 @@
 package de.prog3.projektarbeit.data.objects;
 
 import de.prog3.projektarbeit.data.DataObject;
+import de.prog3.projektarbeit.data.JooqContextProvider;
+import de.prog3.projektarbeit.exceptions.UnableToSaveTeamExeption;
+import org.jooq.DSLContext;
 
 import java.util.HashMap;
+
+import static de.prog3.projektarbeit.data.jooq.tables.Team.TEAM;
 
 
 public class Team extends DataObject {
@@ -14,6 +19,11 @@ public class Team extends DataObject {
         super();
         this.name = name;
         players = new HashMap<>();
+    }
+
+    public Team(int id, String name) {
+        this(name);
+        this.id = id;
     }
 
     public int getId() {
@@ -33,7 +43,17 @@ public class Team extends DataObject {
         return players;
     }
 
-    public void save() {}
+    public void save() throws UnableToSaveTeamExeption {
+        DSLContext ctx = JooqContextProvider.getDSLContext();
+
+        ctx.insertInto(TEAM)
+                .columns(TEAM.NAME)
+                .values(this.name)
+                .onDuplicateKeyUpdate()
+                .set(TEAM.NAME, this.name)
+                .execute();
+        ctx.select(TEAM.ID).from(TEAM).where(TEAM.NAME.eq(this.name)).fetch().forEach(record -> this.id = record.get(TEAM.ID));
+    }
 
     public void setPlayers(HashMap<Integer, Player> players) {
         this.players = players;
