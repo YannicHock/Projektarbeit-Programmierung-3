4a464972e939786c754cc4b082c0f1844420344d
Yannic Hock <yannic.hock@dfki.de>
2025-03-13 18:37:23 +0100

    Verschiedene kleinere Verbesserungen

--- src/main/java/de/prog3/projektarbeit/data/factories/TeamFactory.java
+++ src/main/java/de/prog3/projektarbeit/data/factories/TeamFactory.java
@@ -1,11 +1,14 @@
 package de.prog3.projektarbeit.data.factories;
 
 import de.prog3.projektarbeit.data.JooqContextProvider;
+import de.prog3.projektarbeit.data.objects.Player;
 import de.prog3.projektarbeit.data.objects.Team;
 import de.prog3.projektarbeit.exceptions.TeamNotFoundExeption;
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.Record3;
 import org.jooq.Result;
+import org.jooq.impl.DSL;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,21 +34,35 @@ public class TeamFactory {
         return this;
     }
 
-    public static HashMap<Integer, Team> getAll() {
+    public static HashMap<Integer, Team> getAllWithoutPlayers() {
         DSLContext ctx = JooqContextProvider.getDSLContext();
-        Result<Record> result = ctx.select().from(TEAM).leftJoin(PLAYER).on(PLAYER.TEAM_ID.eq(TEAM.ID)).fetch();
+        Result<Record3<Integer, String, Integer>> result = ctx.select(TEAM.ID, TEAM.NAME, DSL.count(PLAYER.TEAM_ID))
+                .from(TEAM)
+                .leftJoin(PLAYER).on(PLAYER.TEAM_ID.eq(TEAM.ID))
+                .groupBy(TEAM.ID)
+                .fetch();
         HashMap<Integer, Team> teams = new HashMap<>();
         for (Record record : result) {
-            Team team;
-            if (teams.containsKey(record.get(TEAM.ID))) {
-                team = teams.get(record.get(TEAM.ID));
-            } else {
-                team = new Team(record.get(TEAM.ID), record.get(TEAM.NAME));
-                teams.put(record.get(TEAM.ID), team);
-            }
-            PlayerFactory.extractPlayerFromRecord(record).ifPresent(team::addPlayer);
+            Team team = new Team(record.get(TEAM.ID), record.get(TEAM.NAME), record.get(DSL.count(PLAYER.TEAM_ID)));
+            teams.put(team.getId(), team);
         }
         return teams;
+    }
+
+    public static Team getTeamById(int id) throws TeamNotFoundExeption {
+        DSLContext ctx = JooqContextProvider.getDSLContext();
+        Result<Record> result = ctx.select().from(TEAM).leftJoin(PLAYER).on(PLAYER.TEAM_ID.eq(TEAM.ID)).where(TEAM.ID.eq(id)).fetch();
+        if(result == null) {
+            throw new TeamNotFoundExeption("Team mit der ID " + id + " nicht gefunden");
+        }
+        Team team = null;
+        for(Record r : result){
+            if(team == null) {
+                team = new Team(r.get(TEAM.ID), r.get(TEAM.NAME));
+            }
+            PlayerFactory.extractPlayerFromRecord(r).ifPresent(team::addPlayer);
+        }
+        return team;
     }
 
     public static String getNameById(int id) throws TeamNotFoundExeption {
