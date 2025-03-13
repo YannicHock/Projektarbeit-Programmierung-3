0b9a353c0917e2d36902ddce575c78ddfbc68ab2
Yannic Hock <yannic.hock@dfki.de>
2025-03-13 13:29:00 +0100

    Player edit fertig implementiert

--- src/main/java/de/prog3/projektarbeit/data/DataListener.java
+++ src/main/java/de/prog3/projektarbeit/data/DataListener.java
@@ -5,10 +5,13 @@ import de.prog3.projektarbeit.data.factories.TeamFactory;
 import de.prog3.projektarbeit.data.objects.Player;
 import de.prog3.projektarbeit.data.objects.Team;
 import de.prog3.projektarbeit.eventHandling.events.data.player.AttemptPlayerCreationEvent;
+import de.prog3.projektarbeit.eventHandling.events.data.player.AttemptPlayerUpdateEvent;
 import de.prog3.projektarbeit.eventHandling.events.data.player.PlayerCreationFinishedEvent;
+import de.prog3.projektarbeit.eventHandling.events.data.player.PlayerUpdateFinishedEvent;
 import de.prog3.projektarbeit.eventHandling.events.data.team.AttemptTeamCreationEvent;
 import de.prog3.projektarbeit.eventHandling.events.data.team.TeamCreationFinishedEvent;
 import de.prog3.projektarbeit.eventHandling.listeners.data.player.AttemptPlayerCreationListener;
+import de.prog3.projektarbeit.eventHandling.listeners.data.player.AttemptPlayerUpdateListener;
 import de.prog3.projektarbeit.eventHandling.listeners.data.team.AttemptTeamCreationListener;
 import de.prog3.projektarbeit.exceptions.UnableToSavePlayerExeption;
 import de.prog3.projektarbeit.exceptions.UnableToSaveTeamExeption;
@@ -74,6 +77,38 @@ public class DataListener {
                     }
                 }
                 new PlayerCreationFinishedEvent(exceptions).call();
+            }
+        };
+
+        new AttemptPlayerUpdateListener(){
+            @Override
+            public void onEvent(AttemptPlayerUpdateEvent event) {
+                Player player = null;
+                ArrayList<Exception> exceptions = new ArrayList<>();
+                try {
+                    player = new PlayerFactory()
+                            .setFirstName(event.getFirstName())
+                            .setLastName(event.getLastName())
+                            .setPositions(event.getPositions())
+                            .setDateOfBirth(Player.parseStringToDate(event.getDateOfBirth()))
+                            .setNumber(Integer.parseInt(event.getNumber()))
+                            .setId(event.getPlayer().getId())
+                            .setTeamId(event.getPlayer().getTeamId())
+                            .build();
+                } catch (ParseException e) {
+                    exceptions.add(new ParseException("Fehler beim Parsen des Geburtsdatums", e.getErrorOffset()));
+                }
+
+                if(player!=null){
+                    try {
+                        player.save();
+                        new PlayerUpdateFinishedEvent(player, event.getPlayer()).call();
+                        return;
+                    } catch (UnableToSavePlayerExeption e) {
+                        exceptions.add(e);
+                    }
+                }
+                new PlayerUpdateFinishedEvent(exceptions).call();
             }
         };
     }
