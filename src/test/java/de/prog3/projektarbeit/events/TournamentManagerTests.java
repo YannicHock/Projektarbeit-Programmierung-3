3b5b10704fbe4192c3bbfcefbc9ba3a563ed0b1c
YannicHock <mail@yannic-hock.de>
2025-03-21 00:41:02 +0100

    Small refactor

--- src/test/java/de/prog3/projektarbeit/events/TournamentManagerTests.java
+++ src/test/java/de/prog3/projektarbeit/events/TournamentManagerTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class TounamentManagerTest {
+public class TournamentManagerTests {
     private TournamentManager tournamentManager;
 
     @BeforeEach
