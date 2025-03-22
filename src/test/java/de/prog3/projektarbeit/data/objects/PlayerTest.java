4b6b82c93a21cb9fbacbf423989832ae6905f33d
ashishpuru <ashishpuru001@gmail.com>
2025-03-22 19:57:05 +0100

    Add more tests

--- src/test/java/de/prog3/projektarbeit/data/objects/PlayerTest.java
+++ src/test/java/de/prog3/projektarbeit/data/objects/PlayerTest.java
@@ -3,6 +3,9 @@ package de.prog3.projektarbeit.data.objects;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,5 +22,27 @@ public class PlayerTest {
         assertEquals(24,testPlayer1.getNumber());
         assertEquals(250,testPlayer1.getId());
 
+    }
+
+    @Test
+    void testGetAge() {
+        Date birthDate = Date.from(Instant.parse("2000-01-01T00:00:00Z"));
+        Player testPlayer = new Player(250, "Ashish", "Kumar", birthDate, 24, new ArrayList<>(List.of(CM)), 450);
+        int expectedAge = Period.between(birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now()).getYears();
+        assertEquals(expectedAge, testPlayer.getAge());
+    }
+
+    @Test
+    void testFullName() {
+        Date birthDate = Date.from(Instant.parse("2000-01-01T00:00:00Z"));
+        Player testPlayer = new Player(250, "Ashish", "Kumar", birthDate, 24, new ArrayList<>(List.of(CM)), 450);
+        assertEquals("Ashish Kumar", testPlayer.getFullName());
+    }
+
+    @Test
+    void testGetPositionsAsString() {
+        Player testPlayer = new Player(250, "Ashish", "Kumar", Date.from(Instant.now()), 24, new ArrayList<>(List.of(CM)), 450);
+        String expectedPositions = "- " + CM.getFriendlyName();
+        assertEquals(expectedPositions, testPlayer.getPositionsAsString());
     }
 }
