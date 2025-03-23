90a1a86829b3035176cbff21591df246aa1625c9
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-03-23 10:10:52 +0100

    ich habe jetzt die Logik für die Liga mit den zwei Letzten Methode in Tournament Implementiert

--- src/main/java/de/prog3/projektarbeit/data/objects/Tournament.java
+++ src/main/java/de/prog3/projektarbeit/data/objects/Tournament.java
@@ -1,12 +1,17 @@
 package de.prog3.projektarbeit.data.objects;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class Tournament {
         private final String name;
         private final List<Team> teams;
         private final List<Match> matches;
+        private List<Match> FirtsLegMatches;
+        private List<Match> SecondLegMatches;
+
 
         public Tournament(String name) {
             this.name = name;
@@ -36,6 +41,28 @@ public class Tournament {
 
         public void removeTeam(Team team) {
             this.teams.remove(team);
+        }
+
+        public void firstMatchLeg(){
+            for (int i = 0; i < teams.size(); i++){
+                for (int j = i + 1; j < teams.size(); j++){
+                    FirtsLegMatches.add(new Match(teams.get(i), teams.get(j),MatchDate()));
+                }
+            }
+            Collections.shuffle(matches);
+        }
+
+        public void secondMatchLeg(){
+            for (int i = 0; i < teams.size(); i++){
+                for (int j = i + 1; j < teams.size(); j++){
+                    SecondLegMatches.add(new Match(teams.get(j), teams.get(i),MatchDate()));
+                }
+            }
+            Collections.shuffle(matches);
+        }
+
+        public Date MatchDate(){
+            return new Date();
         }
     }
 
