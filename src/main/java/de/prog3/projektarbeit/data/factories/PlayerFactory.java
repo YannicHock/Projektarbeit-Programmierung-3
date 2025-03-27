5931c5259a21b606e4c35a1ac28ef7bd42efc430
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-03-27 10:07:55 +0100

    Docs für Factory erstellt

--- src/main/java/de/prog3/projektarbeit/data/factories/PlayerFactory.java
+++ src/main/java/de/prog3/projektarbeit/data/factories/PlayerFactory.java
@@ -7,6 +7,12 @@ import de.prog3.projektarbeit.exceptions.ValidationException;
 import java.util.ArrayList;
 import java.util.Date;
 
+
+/**
+ * Die Klasse PlayerFactory ist verantwortlich für die Erstellung von Player-Objekten.
+ * Sie bietet Methoden zum Setzen verschiedener Attribute eines Players und validiert die Daten vor der Erstellung des Players.
+ */
+
 public class PlayerFactory {
 
     private String firstName;
@@ -17,41 +23,86 @@ public class PlayerFactory {
     private int id = 0;
     private int teamId = 0;
 
+
+    /**
+     * Setzt das Geburtsdatum des Players.
+     * @param dateOfBirth das Geburtsdatum
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
         return this;
     }
 
+    /**
+     * Setzt die ID des Players.
+     * @param id die ID
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setId(int id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * Setzt die Team-ID des Players.
+     * @param teamId die Team-ID
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setTeamId(int teamId) {
         this.teamId = teamId;
         return this;
     }
 
+    /**
+     * Setzt die Positionen des Players.
+     * @param positions die Positionen
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setPositions(ArrayList<Position> positions) {
         this.positions = positions;
         return this;
     }
 
+    /**
+     * Setzt den Vornamen des Players.
+     * @param firstName der Vorname
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setFirstName(String firstName) {
         this.firstName = firstName;
         return this;
     }
 
+    /**
+     * Setzt den Nachnamen des Players.
+     * @param lastName der Nachname
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setLastName(String lastName) {
         this.lastName = lastName;
         return this;
     }
 
+    /**
+     * Setzt die Nummer des Players.
+     * @param number die Nummer
+     * @return die PlayerFactory-Instanz
+     */
     public PlayerFactory setNumber(int number) {
         this.number = number;
         return this;
     }
 
+    /**
+     * Validiert die Daten zur Erstellung eines Players.
+     * @param firstName der Vorname
+     * @param lastName der Nachname
+     * @param dateOfBirth das Geburtsdatum
+     * @param number die Nummer
+     * @param positions die Positionen
+     * @throws ValidationException wenn eine Validierung fehlschlägt
+     */
     public static void validateData(String firstName, String lastName, Date dateOfBirth, int number, ArrayList<Position> positions) throws ValidationException {
         ArrayList<Exception> exceptions = new ArrayList<>();
         if (firstName == null || firstName.isBlank()) {
@@ -74,6 +125,11 @@ public class PlayerFactory {
         }
     }
 
+    /**
+     * Erstellt und gibt ein Player-Objekt zurück.
+     * @return das Player-Objekt
+     * @throws ValidationException wenn eine Validierung fehlschlägt
+     */
     public Player build() throws ValidationException {
         validateData(firstName, lastName, dateOfBirth, number, positions);
         return new Player(id, firstName, lastName, dateOfBirth, number, positions, teamId);
--- src/main/java/de/prog3/projektarbeit/data/factories/TeamFactory.java
+++ src/main/java/de/prog3/projektarbeit/data/factories/TeamFactory.java
@@ -2,10 +2,18 @@ package de.prog3.projektarbeit.data.factories;
 
 import de.prog3.projektarbeit.data.objects.Team;
 
+/**
+ * Die Klasse TeamFactory ist verantwortlich für die Erstellung von Team-Objekten.
+ * Sie bietet Methoden zum Setzen verschiedener Attribute eines Teams und validiert die Daten vor der Erstellung des Teams.
+ */
 public class TeamFactory {
 
     private String name;
 
+    /**
+     * Setzt den Namen des Teams.
+     * @return die TeamFactory-Instanz
+     */
     public Team build() throws IllegalArgumentException {
         if(name == null || name.isEmpty() || name.isBlank()){
             throw new IllegalArgumentException("Fehlender Teamname");
@@ -14,6 +22,11 @@ public class TeamFactory {
         return new Team(name);
     }
 
+    /**
+     * Setzt den Namen des Teams.
+     * @param name der Name
+     * @return die TeamFactory-Instanz
+     */
     public TeamFactory setName(String name) {
         this.name = name;
         return this;
