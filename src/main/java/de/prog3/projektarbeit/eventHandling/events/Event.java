8bdfc2facbeb494c661673386bddcd502a5d47f6
MathuSpavan <Mathusanlondon@gmail.com>
2025-03-28 01:58:41 +0530

    Dokumentation für Events erweitert

--- src/main/java/de/prog3/projektarbeit/eventHandling/events/Event.java
+++ src/main/java/de/prog3/projektarbeit/eventHandling/events/Event.java
@@ -2,18 +2,39 @@ package de.prog3.projektarbeit.eventHandling.events;
 
 import de.prog3.projektarbeit.eventHandling.EventHandler;
 
+/**
+ * Basisklasse für Events, die vom {@link EventHandler} verarbeitet werden können.
+ * Ein Event besitzt einen Namen und kann über die Methode {@link #call()} ausgelöst werden.
+ * Da diese Klasse abstrakt ist, können nur abgeleitete Klassen instanziiert werden.
+ */
 public abstract class Event {
 
+    /**
+     * Der Name des Events, z. B. "PlayerCreationEvent".
+     */
     private final String name;
 
+    /**
+     * Erstellt ein neues Event mit dem angegebenen Namen.
+     *
+     * @param name Eindeutige Bezeichnung für das Event.
+     */
     public Event(String name) {
         this.name = name;
     }
 
+    /**
+     * Gibt den Namen des Events zurück.
+     *
+     * @return Name des Events.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Löst das Event aus, sodass es vom {@link EventHandler} verarbeitet werden kann.
+     */
     public void call() {
         EventHandler.getInstance().callEvent(this);
     }
